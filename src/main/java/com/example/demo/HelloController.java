2c34a840b42f1d52a193bcbaf7cc5d99b7074ae5
Malichenko-Maksym <s221680@student.uek.krakow.pl>
2023-03-19 16:17:14 +0100

    Fixed bug with exchange rate

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,20 +1,30 @@
 package com.example.demo;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
-    private double exchangeRate = WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json");
+    private double exchangeRate;
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
 
     @GetMapping
     public String mainPage(Model model) {
-        model.addAttribute("exchangeRate", exchangeRate);
+        setExchangeRate(WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json"));
+        model.addAttribute("exchangeRate", getExchangeRate());
         return "main";
     }
 }
