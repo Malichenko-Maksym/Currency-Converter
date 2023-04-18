1068c6344e74dba562113e0348da113a70e69171
Maksym Malichenko <s221680@student.uek.krakow.pl>
2023-04-18 20:05:39 +0200

    18.04

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -11,6 +13,17 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
+    String[] currencyArray = {
+            "http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json",
+            "http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json",
+            "http://api.nbp.pl/api/exchangerates/rates/a/jpy/?format=json",
+            "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json",
+            "http://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json",
+           // "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json"
+    };
+    String curr = "jpy";
+    String testLink = "http://api.nbp.pl/api/exchangerates/rates/a/"+curr+"/?format=json";
 
     private double exchangeRate;
     public double getExchangeRate() {
@@ -23,8 +36,17 @@ public class HelloController {
 
     @GetMapping
     public String mainPage(Model model) {
-        setExchangeRate(WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json"));
+        setExchangeRate(WebRequest.getRateFrom(testLink));
         model.addAttribute("exchangeRate", getExchangeRate());
+        model.addAttribute("cur", new String());
         return "main";
     }
+
+    @PostMapping("/firstCur")
+    public String firstCur(@ModelAttribute String cur){
+
+        System.out.println(cur);
+        return "main";
+    }
+
 }
