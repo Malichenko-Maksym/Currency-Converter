1398851cd707c6a63e171252a673c6fffe4567dd
Maksym Malichenko <s221680@student.uek.krakow.pl>
2023-04-30 00:20:20 +0200

    MultiCurrencyAvailable

---
+++ src/main/java/com/example/demo/AcceptedResponce.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class AcceptedResponce {
+    public String getChosenFirstCur() {
+        return chosenFirstCur;
+    }
+
+    public void setChosenFirstCur(String chosenFirstCur) {
+        this.chosenFirstCur = chosenFirstCur;
+    }
+
+    private String chosenFirstCur;
+
+    public String getChosenSecondCur() {
+        return chosenSecondCur;
+    }
+
+    public void setChosenSecondCur(String chosenSecondCur) {
+        this.chosenSecondCur = chosenSecondCur;
+    }
+
+    private String chosenSecondCur;
+}
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,51 +2,46 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class HelloController{
 
-    String[] currencyArray = {
-            "http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json",
-            "http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json",
-            "http://api.nbp.pl/api/exchangerates/rates/a/jpy/?format=json",
-            "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json",
-            "http://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json",
-           // "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json"
-    };
-    String curr = "jpy";
-    String testLink = "http://api.nbp.pl/api/exchangerates/rates/a/"+curr+"/?format=json";
-
-    private double exchangeRate;
-    public double getExchangeRate() {
-        return exchangeRate;
-    }
-
-    public void setExchangeRate(double exchangeRate) {
-        this.exchangeRate = exchangeRate;
-    }
 
     @GetMapping
     public String mainPage(Model model) {
-        setExchangeRate(WebRequest.getRateFrom(testLink));
-        model.addAttribute("exchangeRate", getExchangeRate());
         model.addAttribute("cur", new String());
         return "main";
     }
 
-    @PostMapping("/firstCur")
-    public String firstCur(@ModelAttribute String cur){
-
-        System.out.println(cur);
-        return "main";
+    @PostMapping("/submitFirstCur")
+    @ResponseBody
+    public double submitFirstCur(@RequestBody AcceptedResponce accRes){
+        String firstCur = accRes.getChosenFirstCur();
+        Double firstCurRate;
+        if(firstCur.equals("pln")) {
+            firstCurRate=1.0;
+        } else {
+            firstCurRate = WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/"+firstCur+"/?format=json");
+        }
+        return firstCurRate;
     }
 
+    @PostMapping("/submitSecondCur")
+    @ResponseBody
+    public double submitSecondCur(@RequestBody AcceptedResponce accRes){
+        String secondCur = accRes.getChosenSecondCur();
+        Double secondCurRate;
+        if(secondCur.equals("pln")) {
+            secondCurRate=1.0;
+        } else {
+            secondCurRate = WebRequest.getRateFrom("http://api.nbp.pl/api/exchangerates/rates/a/"+secondCur+"/?format=json");
+        }
+        return secondCurRate;
+    }
 }
+
