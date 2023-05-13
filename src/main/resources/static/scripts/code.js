3aa50091b16029ebb52559de97643957c4586d77
Maksym Malichenko <s221680@student.uek.krakow.pl>
2023-05-13 03:15:38 +0200

    final

--- src/main/resources/static/scripts/code.js
+++ src/main/resources/static/scripts/code.js
@@ -1,4 +1,5 @@
 var request = new XMLHttpRequest();
+// variables for exchange rates, where all exchange rates are provided in zł
 var ex1;
 var ex2;
 
@@ -8,11 +9,14 @@ let secondCurSelection = document.getElementById('secondCur');
 let firstCurInput = document.getElementById('firstCurInp');
 let secondCurInput  = document.getElementById('secondCurInp');
 
+let replaceButton = document.getElementById('replaceButton');
+
 firstCurInput.addEventListener('input', calculateSecondCur);
 secondCurInput.addEventListener('input', calculateFirstCur);
 
 firstCurInput.hidden  = true;
 secondCurInput.hidden  = true;
+replaceButton.hidden = true;
 
 function regExCheckKeydown(event,elemId) {
     let currentElem = document.getElementById(elemId);
@@ -41,7 +45,7 @@ function getExchangeRate(chosenSelector) {
     request.send(JSON.stringify({chosenFirstCur : chosenSelector.value}));
     return answer;
 }
-
+//exchange rate between currencies none of which is zł, is calculated based on the exchange rate to the zloty of each of them
 function showNotation(){
     if (firstCurSelection.value!="none" && secondCurSelection.value!="none"){
         let firstText = firstCurSelection.options[firstCurSelection.selectedIndex].text;
@@ -52,6 +56,7 @@ function showNotation(){
         document.getElementById("fee").style.visibility = "visible";
         firstCurInput.hidden  = false;
         secondCurInput.hidden  = false;
+        replaceButton.hidden = false;
     }
 }
 
@@ -69,17 +74,36 @@ function calculateSecondCur() {
     }
 }
 
+function selected(mainSelection, anoutherSelection){
+    for (var i=0; i<anoutherSelection.length; i++) {
+        if (anoutherSelection.options[i].value == mainSelection.value) {
+            anoutherSelection.options[i].style.display = 'none';
+        } else {
+            anoutherSelection.options[i].style.display = 'inline';
+        }
+    }
+    showNotation();
+    calculateSecondCur();
+}
+
 function firstSelected(){
     ex1=getExchangeRate(firstCurSelection);
-    showNotation();
-    if(secondCurInput.value.length>0){
-        calculateSecondCur();
-    }
+    selected(firstCurSelection, secondCurSelection);
 }
 function secondSelected(){
     ex2=getExchangeRate(secondCurSelection);
-    showNotation();
-    if(firstCurInput.value.length>0){
-        calculateSecondCur();
-    }
+    selected(secondCurSelection,firstCurSelection);
 }
+
+function replaceCurrency(){
+    let inputBuffer = firstCurInput.value;
+    firstCurInput.value = secondCurInput.value;
+    secondCurInput.value = inputBuffer;
+
+    let selectionBuffer = firstCurSelection.value;
+    firstCurSelection.value = secondCurSelection.value;
+    secondCurSelection.value = selectionBuffer;
+    firstSelected();
+    secondSelected();
+}
+
